004da7c81a33f1013f35fcb76f29fda868c312a7
seers <seers@live.com>
2024-02-28 22:43:52 +0800

    remove

--- dump_SO.js
+++
@@ -1,9 +0,0 @@
-const lib = '';
-const pkg = '';
-
-let m = Process.getModuleByName(lib);
-let file = new File('/data/data/' + pkg + '/files/' + m.base + '.' + lib, 'wb');
-Memory.protect(m.base, m.size, 'rw-');
-file.write(m.base.readByteArray(m.size));
-file.flush();
-file.close();
