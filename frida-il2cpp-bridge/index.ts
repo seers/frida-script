7de99c892428d618571f6fdf5bfe7560b07a6f47
seers <seers@live.com>
2024-02-29 20:33:16 +0800

    add trace class

--- frida-il2cpp-bridge/index.ts
+++ frida-il2cpp-bridge/index.ts
@@ -5,34 +5,35 @@ Il2Cpp.perform(() => {
     // Il2Cpp.dump();
 
     //Trace
+    // Il2Cpp.trace()
+    //     .assemblies(Il2Cpp.domain.assembly("Heroes"))
+    //     .and()
+    //     .attach();
+
+    //Trace class
+    // const CFormula = Il2Cpp.domain.assembly("Fight").image
+    //     .class("Fight.CFormula")
     // Il2Cpp.trace(true)
-    //     .assemblies(Il2Cpp.domain.assembly("Utilites"))
+    //     .classes(CFormula)
     //     .and()
     //     .attach();
 
     //Trace method
-    // const AESEncrypt = Il2Cpp.domain.assembly("Utilites").image
-    //     .class("CornaSpace.AESHelper")
-    //     .method("AESEncrypt");
-
+    // const GetHp = Il2Cpp.domain.assembly("Heroes").image
+    //     .class("Heroes.CMonster")
+    //     .method("GetHp");
     // Il2Cpp.trace(true)
-    //     .methods(AESEncrypt)
+    //     .methods(GetHp)
     //     .and()
     //     .attach();
 
     //Replacement
-    // const AESEncrypt = Il2Cpp.domain.assembly("Utilites").image
-    //     .class("CornaSpace.AESHelper")
-    //     .method<Il2Cpp.String>("AESEncrypt");
-
-    // AESEncrypt.implementation = function (text: Il2Cpp.String, bCanChange: boolean): Il2Cpp.String {
-    //     // console.log('text:', text);
-    //     // console.log('bCanChange:', bCanChange);
-    //     if (text.content == "20030") {
-    //         text = Il2Cpp.string("29999");
-    //     }
-    //     const result = this.method<Il2Cpp.String>("AESEncrypt").invoke(text, bCanChange);
-    //     // console.log('result:', result);
+    // const GetHp = Il2Cpp.domain.assembly("Heroes").image
+    //     .class("Heroes.CMonster")
+    //     .method<number>("GetHp");
+    // GetHp.implementation = function (): number {
+    //     const result = this.method<number>("GetHp").invoke();
+    //     console.log('result:',result);
     //     return result;
     // };
 });
