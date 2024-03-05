ecb280d5e39c30047cfbab1512774be5a166096a
seers <seers@live.com>
2024-03-05 22:20:34 +0800

    Since we push file to sdcard, we dont need to pay attention to the file permission

--- push.bat
+++
@@ -1,3 +0,0 @@
-adb push index.jsc /sdcard/Download/
-adb shell "su -c mv /sdcard/Download/index.jsc /data/data/com.chuanhuo.mud.test/files/"
-adb shell "su -c chown u0_a25:u0_a25 /data/data/com.chuanhuo.mud.test/files/index.jsc"
